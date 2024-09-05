1b8a70c8b8d9fd13afe5b8ba709abcc9c2d2b260
prasad <prasad@gmail.com>
2024-09-05 17:52:54 +0530

    k8-terraform

---
+++ 20-bastion/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+  backend "s3" {
+    bucket = "prasad-remote-state"
+    key    = "expense-dev-bastion"
+    region = "us-east-1"
+    dynamodb_table = "eks-infra-locking"
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ 50-acm/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.62.0"
+    }
+  }
+  backend "s3" {
+    bucket = "prasad-remote-state"
+    key    = "expense-dev-acm"
+    region = "us-east-1"
+    dynamodb_table = "eks-infra-locking"
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
